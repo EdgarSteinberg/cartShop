3b0a4ea833c22c20dfb76c2456c84015c327ed3c
¨Edgar <s.steinberg2019@gmail.com>
2024-01-08 14:23:43 -0300

    responsivo

--- src/components/itemDetail/itemDetail.tsx
+++ src/components/itemDetail/itemDetail.tsx
@@ -1,23 +1,26 @@
 import React, { useContext, useState } from "react";
 import Producto from "../../types/types";
 import Counter from "../counter/counter";
-import styles from "./styles.module.css"
+import styles from "./styles.module.css";
 import { CartContext } from "../../context/cartContext";
 
-
 const ItemDetail: React.FC<{ item: Producto }> = ({ item }) => {
-
-    const { agregarAlCarrito } = useContext(CartContext)
-
-    const [cantidad, setCantidad] = useState<number>(1)
+    const { agregarAlCarrito: agregarProductoAlCarrito } = useContext(CartContext);
+    const [mostrar, setMostrar] = useState(true);
+    const [cantidad, setCantidad] = useState<number>(1);
 
     const handleSumar = () => {
-        cantidad < item.stock && setCantidad(cantidad + 1)
-    }
-    const handleRestar = () => {
-        (cantidad > 1) && setCantidad(cantidad - 1)
-    }
+        cantidad < item.stock && setCantidad(cantidad + 1);
+    };
 
+    const handleRestar = () => {
+        cantidad > 1 && setCantidad(cantidad - 1);
+    };
+
+    const agregarItemAlCarrito = () => {
+        agregarProductoAlCarrito(item, cantidad);
+        setMostrar(false); // Oculta el contador después de agregar al carrito
+    };
 
     return (
         <>
@@ -28,11 +31,14 @@ const ItemDetail: React.FC<{ item: Producto }> = ({ item }) => {
                     <p>{item.descripcion}</p>
                     <h1> ${item.precio}</h1>
                     <p>{item.categoria}</p>
-                    <Counter
-                        cantidad={cantidad}
-                        handleSumar={handleSumar}
-                        handleRestar={handleRestar}
-                        handleAgregar={() => { agregarAlCarrito(item, cantidad) }} />
+                    {cantidad > 0 && mostrar && (
+                        <Counter
+                            cantidad={cantidad}
+                            handleSumar={handleSumar}
+                            handleRestar={handleRestar}
+                            handleAgregar={agregarItemAlCarrito}
+                        />
+                    )}
                 </div>
             </div>
         </>
